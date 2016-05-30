ffd00304754356ed4cb8ef57f4a2b777d238d325
Qiang Luo <hzluoqiang@corp.netease.com>
2016-05-30 14:29:04 +0800

    add pair feature

--- data/__init__.py
+++ data/__init__.py
@@ -1,12 +1,5 @@
-import pandas as pd
 import os
 import pickle
-import json
-
-__all__ = ['item_info_train', 'item_pairs_train',
-           'item_info_test', 'item_pairs_test',
-           'category_parent', 'location_region'
-           ]
 
 dir_here = os.path.dirname(os.path.realpath(__file__))
 data_file_dir = os.path.join(dir_here, 'data_files')
@@ -15,7 +8,7 @@ cache_dir = os.path.join(dir_here, 'cache')
 
 def with_cache(cache_file, g):
     if os.path.exists(cache_file):
-        return pd.read_pickle(cache_file)
+        return pickle.load(open(cache_file, 'rb'))
     else:
         res = g()
         pickle.dump(res, open(cache_file, 'wb'),
@@ -28,18 +21,4 @@ def load_file_with_cache(name, reader, f):
                       lambda: reader(os.path.join(data_file_dir, f)))
 
 
-def read_item_info(f):
-    res = pd.read_csv(f, index_col='itemID')
-    res['images_array'] = res['images_array'].apply(lambda x: list(map(int, x.split(','))) if pd.notnull(x) else [])
-    res['attrsJSON'] = res['attrsJSON'].apply(lambda x: json.loads(x) if isinstance(x, str) else {})
-
-    return res
-
-
-item_info_test = load_file_with_cache('item_info_test', read_item_info, 'ItemInfo_test.csv')
-item_info_train = load_file_with_cache('item_info_train', read_item_info, 'ItemInfo_train.csv')
-item_pairs_test = load_file_with_cache('item_pairs_test', pd.read_csv, 'ItemPairs_test.csv')
-item_pairs_train = load_file_with_cache('item_pairs_train', pd.read_csv, 'ItemPairs_train.csv')
-category_parent = load_file_with_cache('category_parent_dict', pd.read_csv, 'Category.csv')
-location_region =load_file_with_cache('location_region', pd.read_csv, 'Location.csv')
-
+from .original import *
---
+++ data/pair_feature.py
@@ -0,0 +1,39 @@
+from data import *
+import pandas as pd
+
+__all__ = ['gen_simple_feature', 'gen_features']
+
+
+def jaccard(a, b):
+    a = set(a)
+    b = set(b)
+    n_intersection = len(a.intersection(b))
+    n_union = len(a) + len(b) - n_intersection
+    if n_union == 0:
+        return 0.0
+    else:
+        return n_intersection / n_union
+
+
+def gen_simple_feature(a, b):
+    feats = {}
+    for name in ['title', 'description', 'categoryID', 'locationID', 'metroID']:
+        feats['same_' + name] = a[name] == b[name]
+    feats['same_lat_lon'] = (a['lat'] == b['lat']) and (a['lon'] == b['lon'])
+
+    price_diff = abs(a['price'] - b['price'])
+    feats['price_diff_ratio'] = price_diff / (a['price'] + b['price'])
+
+    feats['attrsJSON_key_jaccard'] = jaccard(a['attrsJSON'].keys(), b['attrsJSON'].keys())
+    feats['attrsJSON_item_jaccard'] = jaccard(a['attrsJSON'].items(), b['attrsJSON'].items())
+
+    return feats
+
+
+def gen_features(g, pairs):
+    X = []
+    for i, j in zip(pairs['itemID_1'], pairs['itemID_2']):
+        a = get_item(i)
+        b = get_item(j)
+        X.append(g(a, b))
+    return pd.DataFrame(X)
