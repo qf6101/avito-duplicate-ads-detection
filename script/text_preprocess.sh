517d52f946597ce47819ba2279a16cecb71d032a
Qiang Luo <hzluoqiang@corp.netease.com>
2016-06-03 18:52:16 +0800

    merge train and test result

--- script/text_preprocess.sh
+++ script/text_preprocess.sh
@@ -6,5 +6,5 @@ pv --rate -i 5 \
   | parallel --gnu --linebuffer --pipe -N 10  --jobs 16 --round-robin ./feature/text_preprocess.py
 
 }
-cat ./data/data_files/ItemInfo_test.csv | work > ./data/data_files/ItemInfo_test_preprocessed.jsonl
-cat ./data/data_files/ItemInfo_train.csv | work > ./data/data_files/ItemInfo_train_preprocessed.jsonl
+cat ./data/data_files/ItemInfo_test.csv | work > ./data/data_files/ItemInfo_preprocessed.jsonl
+cat ./data/data_files/ItemInfo_train.csv | work >> ./data/data_files/ItemInfo_preprocessed.jsonl
