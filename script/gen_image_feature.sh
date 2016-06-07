95b4ef45d0787c37e2e11522882f7f8908b12359
Qiang Luo <hzluoqiang@corp.netease.com>
2016-06-07 17:08:44 +0800

    no need for separate sort

--- script/gen_image_feature.sh
+++ script/gen_image_feature.sh
@@ -8,13 +8,10 @@
 function work(){
 pv --rate -i 5 \
  | csvcut -c 'index,images_array_1,images_array_2' | csvjson --stream \
-  | parallel --gnu --linebuffer --pipe -N 10  --jobs 16 --round-robin python ../feature/image_feature.py
+  | parallel --gnu -k --pipe -N 10  --jobs 16 --round-robin python ../feature/image_feature.py| in2csv -f ndjson
 
 }
 
 # Generate image feature for training data set and testing data set
-cat ../data/data_files/image_itemPairs_train.csv | work > ../data/data_files/image_feature_train.jsonl
-cat ../data/data_files/image_itemPairs_test.csv | work > ../data/data_files/image_feature_test.jsonl
-# Sort image feature files (json format) and save to csv files
-python ../feature/sort_image_feature.py -f ../data/data_files/image_feature_train.jsonl
-python ../feature/sort_image_feature.py -f ../data/data_files/image_feature_test.jsonl
+cat ../data/data_files/image_itemPairs_train.csv | work > ../data/data_files/image_feature_train.csv
+cat ../data/data_files/image_itemPairs_test.csv | work > ../data/data_files/image_feature_test.csv
