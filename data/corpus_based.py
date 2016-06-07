0930781586d2be428bf70607807ac5ed259d43fb
Qiang Luo <hzluoqiang@corp.netease.com>
2016-06-07 16:19:06 +0800

    cosine similarity; more variants for desc lsa

--- data/corpus_based.py
+++ data/corpus_based.py
@@ -9,7 +9,8 @@ import re
 from itertools import chain, islice
 import json
 
-from . import data_file_dir,  generate_with_cache, get_data_file, get_cache_file
+from . import data_file_dir, generate_with_cache, get_data_file, get_cache_file
+
 # from .original import item_pairs_train, item_pairs_test
 # from .item import item_id_to_index
 
@@ -24,16 +25,46 @@ preprocessed_text = RootNode([preprocessed_text_file])
 dfs_file = get_data_file('df.pickle')
 dfs = RootNode([dfs_file])
 
+item_pairs_train_file = get_cache_file('item_pairs_train.pickle')
+item_pairs_test_file = get_cache_file('item_pairs_train.pickle')
+
+
+class PairRelation(PickleNode):
+    def __init__(self):
+        super(PairRelation, self).__init__(get_cache_file('pair_relation.pickle'),
+                                           [RootNode([item_pairs_train_file, item_pairs_test_file])])
+
+    def compute(self):
+        from .item import item_id_to_index
+        from .original import item_pairs_train, item_pairs_test
+
+        I = []
+        J = []
+        for a, b in chain(zip(item_pairs_train.itemID_1, item_pairs_train.itemID_2),
+                          zip(item_pairs_test.itemID_1, item_pairs_test.itemID_2)):
+            I.append(item_id_to_index[a])
+            J.append(item_id_to_index[b])
+
+        self.pairs = list(zip(I, J))
+
+    def decorate_data(self):
+        return self.pairs
+
+
+pair_relation = PairRelation()
+
+
 def collect_tokens(x):
     res = []
     for y in x:
         res.extend(y)
     return res
 
+
 class DocumentTermMatrix(PickleNode):
     def __init__(self, name, slot, lower=True):
-        super(DocumentTermMatrix, self).__init__(get_cache_file(name+'.pickle'),
-                                               [preprocessed_text, dfs])
+        super(DocumentTermMatrix, self).__init__(get_cache_file(name + '.pickle'),
+                                                 [preprocessed_text, dfs])
         self.name = name
         self.slot = slot
         self.lower = lower
@@ -50,7 +81,6 @@ class DocumentTermMatrix(PickleNode):
         if self.lower:
             words = sorted(set(map(str.lower, words)))
         word_to_index = dict(zip(words, range(len(words))))
-
 
         I = []
         J = []
@@ -69,11 +99,16 @@ class DocumentTermMatrix(PickleNode):
         self.dtm = M
         self.words = words
 
-    def decorate_data(self):
-        return self.words, self.dtm
+    def decorate_data(self, matrix_only=False):
+        if matrix_only:
+            return self.dtm
+        else:
+            return self.words, self.dtm
+
 
 class WordFilter:
     stopwords = set(nltk.corpus.stopwords.words('russian'))
+
     @classmethod
     def contain_alphabet(cls, w, remove_stopwords=True):
         if remove_stopwords:
@@ -84,9 +119,10 @@ class WordFilter:
 
 class DocumentTermMatricFilter(PickleNode):
     def __init__(self, name, src, word_filter):
-        super(DocumentTermMatricFilter, self).__init__(get_cache_file(name+'.pickle'),
-                                           [src])
+        super(DocumentTermMatricFilter, self).__init__(get_cache_file(name + '.pickle'),
+                                                       [src])
         self.word_filter = word_filter
+
     def compute(self):
         words, dtm = self.dependencies[0].get_data()
 
@@ -101,8 +137,13 @@ class DocumentTermMatricFilter(PickleNode):
 
         self.dtm = dtm
         self.words = words
-    def decorate_data(self):
-        return self.words, self.dtm
+
+    def decorate_data(self, matrix_only=False):
+        if matrix_only:
+            return self.dtm
+        else:
+            return self.words, self.dtm
+
 
 class RepresentationModel(PickleNode):
     def __init__(self, name, dtm_node, model, dtm_transformer=None):
@@ -117,58 +158,105 @@ class RepresentationModel(PickleNode):
             dtm = self.dtm_transformer.fit_transform(dtm)
         self.embedding = self.model.fit_transform(dtm)
 
-    def decorate_data(self):
-        return self.embedding, self.model
+    def decorate_data(self, matrix_only=False):
+        if matrix_only:
+            return self.embedding
+        else:
+            return self.embedding, self.model
 
-from sklearn.decomposition import TruncatedSVD, NMF
+from sklearn.preprocessing import Normalizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.preprocessing import Binarizer
+l2_normalizer_inplace = Normalizer(norm='l2', copy=False)
+binarizer = Binarizer()
+tfidf_transformer = TfidfTransformer()
+
+class CosineSimilarityFeature(PickleNode):
+    def __init__(self, name, vec_models):
+        self.vec_models = vec_models
+        super(CosineSimilarityFeature, self).__init__(get_cache_file(name + '.pickle'), vec_models + [pair_relation])
+
+    def compute(self):
+        pairs = pair_relation.get_data()
+        feats = np.empty((len(pairs), 4*len(self.vec_models)))
+        columns = []
+        for j, m in enumerate(self.vec_models):
+            columns.extend(m.name+'__'+version for version in ['tf', 'binary_tf', 'tfidf', 'binary_tfidf'])
+            V0 = m.get_data(matrix_only=True)
+            V1 = binarizer.fit_transform(V0)
+            V2 = tfidf_transformer.fit_transform(V0)
+            V3 = tfidf_transformer.fit_transform(V1)
+            for k,V in enumerate([V0, V1, V2, V3]):
+                l2_normalizer_inplace.fit(V)
+                for i, (a, b) in enumerate(pairs):
+                    va = V[a]
+                    vb = V[b]
+                    feats[i,j*4+k] = va.dot(vb.T)[0,0]
+        self.feats = pd.DataFrame(feats, columns=[name+'__cosine_similarity' for name in columns])
+
+    def decorate_data(self):
+        return self.feats
+
+
+from sklearn.decomposition import TruncatedSVD, NMF
+
 from sklearn.pipeline import Pipeline
 
 title_word_dtm_0 = DocumentTermMatrix('title_word_dtm_0', slot=('word_stemmed_ngram', True, 'title'), lower=True)
 title_word_dtm_1 = DocumentTermMatricFilter('title_word_dtm_1', title_word_dtm_0, WordFilter.contain_alphabet)
-description_word_dtm_0 = DocumentTermMatrix('description_word_dtm_0', slot=('word_stemmed_ngram', True, 'description'), lower=True)
-description_word_dtm_1 = DocumentTermMatricFilter('description_word_dtm_1', description_word_dtm_0, WordFilter.contain_alphabet)
+description_word_dtm_0 = DocumentTermMatrix('description_word_dtm_0', slot=('word_stemmed_ngram', True, 'description'),
+                                            lower=True)
+description_word_dtm_1 = DocumentTermMatricFilter('description_word_dtm_1', description_word_dtm_0,
+                                                  WordFilter.contain_alphabet)
 title_word_lsa_1_0 = RepresentationModel('title_word_lsa_1_0', title_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, n_iter=20, random_state=0),
+                                         model=TruncatedSVD(n_components=100, n_iter=20, random_state=0),
                                          dtm_transformer=Binarizer(copy=False)
                                          )
 title_word_lsa_1_1 = RepresentationModel('title_word_lsa_1_1', title_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, n_iter=20, random_state=1),
+                                         model=TruncatedSVD(n_components=100, n_iter=20, random_state=1),
                                          dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
                                                                    ('tfidf_transformer', TfidfTransformer())])
                                          )
 description_word_lsa_1_0 = RepresentationModel('description_word_lsa_1_0', description_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, n_iter=20, random_state=2),
-                                         dtm_transformer=Binarizer(copy=False)
-                                         )
+                                               model=TruncatedSVD(n_components=100, n_iter=20, random_state=2),
+                                               dtm_transformer=Binarizer(copy=False)
+                                               )
 description_word_lsa_1_1 = RepresentationModel('description_word_lsa_1_1', description_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, n_iter=20, random_state=3),
-                                         dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
-                                                                   ('tfidf_transformer', TfidfTransformer())])
-                                         )
+                                               model=TruncatedSVD(n_components=100, n_iter=20, random_state=3),
+                                               dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
+                                                                         ('tfidf_transformer', TfidfTransformer())])
+                                               )
+description_word_lsa_1_2 = RepresentationModel('description_word_lsa_1_2', description_word_dtm_1,
+                                               model=TruncatedSVD(n_components=100, n_iter=20, random_state=3)
+                                               )
+description_word_lsa_1_3 = RepresentationModel('description_word_lsa_1_3', description_word_dtm_1,
+                                               model=TruncatedSVD(n_components=100, n_iter=20, random_state=3),
+                                               dtm_transformer=TfidfTransformer()
+                                               )
 title_word_nmf_1_0 = RepresentationModel('title_word_nmf_1_0', title_word_dtm_1,
-                                         model = NMF(n_components=100, random_state=4),
+                                         model=NMF(n_components=100, random_state=4),
                                          dtm_transformer=Binarizer(copy=False)
                                          )
 title_word_nmf_1_1 = RepresentationModel('title_word_nmf_1_1', title_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, random_state=5),
+                                         model=TruncatedSVD(n_components=100, random_state=5),
                                          dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
                                                                    ('tfidf_transformer', TfidfTransformer())])
                                          )
 description_word_nmf_1_0 = RepresentationModel('description_word_nmf_1_0', description_word_dtm_1,
-                                         model = NMF(n_components=100, random_state=4),
-                                         dtm_transformer=Binarizer(copy=False)
-                                         )
+                                               model=NMF(n_components=100, random_state=4),
+                                               dtm_transformer=Binarizer(copy=False)
+                                               )
 description_word_nmf_1_1 = RepresentationModel('description_word_nmf_1_1', description_word_dtm_1,
-                                         model = TruncatedSVD(n_components=100, random_state=5),
-                                         dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
-                                                                   ('tfidf_transformer', TfidfTransformer())])
-                                         )
-
-
+                                               model=TruncatedSVD(n_components=100, random_state=5),
+                                               dtm_transformer=Pipeline([('binarizer', Binarizer(copy=False)),
+                                                                         ('tfidf_transformer', TfidfTransformer())])
+                                               )
 
 models = [title_word_dtm_0, title_word_dtm_1, description_word_dtm_0, description_word_dtm_1,
-          title_word_lsa_1_0, title_word_lsa_1_1, description_word_lsa_1_0, description_word_lsa_1_1,
-          title_word_nmf_1_0, title_word_nmf_1_1, description_word_nmf_1_0, description_word_nmf_1_1,
+          title_word_lsa_1_0, title_word_lsa_1_1,
+          description_word_lsa_1_0, description_word_lsa_1_1, description_word_lsa_1_2, description_word_lsa_1_3
+          # title_word_nmf_1_0, title_word_nmf_1_1, description_word_nmf_1_0, description_word_nmf_1_1,
           ]
+cosine_similarity_features = CosineSimilarityFeature('cosine_similarity_features',
+                                                     models)
+feats = [cosine_similarity_features]
