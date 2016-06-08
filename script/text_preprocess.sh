06506e472f46b817a3046ca28b2b6860664c176f
Qiang Luo <hzluoqiang@corp.netease.com>
2016-06-08 09:30:54 +0800

    fix bad order

--- script/text_preprocess.sh
+++ script/text_preprocess.sh
@@ -6,5 +6,5 @@ pv --rate -i 5 \
   | parallel --gnu --pipe -N 1  --jobs 4 --round-robin ./feature/text_preprocess.py
 
 }
-cat ./data/data_files/ItemInfo_test.csv | work > ./data/data_files/ItemInfo_preprocessed.jsonl
-cat ./data/data_files/ItemInfo_train.csv | work >> ./data/data_files/ItemInfo_preprocessed.jsonl
+cat ./data/data_files/ItemInfo_train.csv | work > ./data/data_files/ItemInfo_preprocessed.jsonl
+cat ./data/data_files/ItemInfo_test.csv | work >> ./data/data_files/ItemInfo_preprocessed.jsonl
