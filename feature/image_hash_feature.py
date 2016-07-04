8699e451e6ec276aa7bf76b15eb9c16006e388f5
Qiang Luo <hzluoqiang@corp.netease.com>
2016-07-04 17:01:22 +0800

    add more hashes

--- feature/image_hash_feature.py
+++ feature/image_hash_feature.py
@@ -4,10 +4,13 @@ from util import binary_matrix_to_int, jaccard
 from config import config
 from sklearn.metrics.pairwise import pairwise_distances
 import numpy as np
+
 images_dir = config['image_root']
 
 import logging
+
 logger = logging.getLogger('image_hash_feature')
+
 
 def image_path(image_id):
     """
@@ -17,6 +20,11 @@ def image_path(image_id):
     second_index = str(int(image_id) % 100)
     return ''.join([images_dir, "/Images_", first_index, "/", second_index, "/", str(image_id).strip(), ".jpg"])
 
+
+hash_names = ['ahash', 'phash', 'dhash', 'whash']
+hash_functions = [imagehash.average_hash, imagehash.phash, imagehash.dhash, imagehash.whash]
+
+
 def dhash(image_id):
     try:
         image = Image.open(image_path(image_id))
@@ -24,10 +32,18 @@ def dhash(image_id):
         logger.warn('cannot find image {}'.format(image_id))
         return None
     else:
-        return imagehash.phash(image).hash.ravel()
+        return np.stack(f(image).hash.ravel() for f in hash_functions)
 
 
-headers = ['index', 'image_dhash_jaccard'] + ['image_dhash_hamming_{}_{}'.format(x,y) for x in [0,1] for y in ['min', 'max', 'mean']]
+def build_headers():
+    headers = ['index']
+    for hash_name in hash_names:
+        headers.append('image_{}_jaccard'.format(hash_name))
+        headers.extend(['image_{}_hamming_{}_{}'.format(hash_name, x, y) for x in [0, 1] for y in ['min', 'max', 'mean']])
+    return headers
+headers = build_headers()
+
+
 def pair_features(hashes1, hashes2):
     feats = [jaccard(binary_matrix_to_int(hashes1), binary_matrix_to_int(hashes2))]
 
@@ -42,23 +58,30 @@ def pair_features(hashes1, hashes2):
         feats.extend([s0.min(), s0.max(), s0.mean(), s1.min(), s1.max(), s1.mean()])
     return feats
 
+
 def _collect_hashes(I):
     hashes = list(filter(lambda x: x is not None, (dhash(i) for i in I)))
     if len(hashes) == 0:
         return None
     else:
-        return np.vstack(hashes)
+        return np.stack(hashes)
+
 
 def gen_features(I1, I2):
+    n_features = 7 * len(hash_functions)
     if len(I1) == 0 or len(I2) == 0:
-        return [np.nan] * 7
+        return [np.nan] * n_features
     else:
         H1 = _collect_hashes(I1)
         H2 = _collect_hashes(I2)
         if H1 is not None and H2 is not None:
-            return pair_features(H1, H2)
+            features = []
+            for x, y in zip(np.transpose(H1, (1, 0, 2)), np.transpose(H2, (1, 0, 2))):
+                features.extend(pair_features(x, y))
+            return features
         else:
-            return [np.nan] * 7
+            return [np.nan] * n_features
+
 
 def parse_image_array(x):
     if len(x) == 0:
@@ -66,15 +89,17 @@ def parse_image_array(x):
     else:
         return list(map(int, x.split(', ')))
 
+
 if __name__ == '__main__':
     import sys
     import json
+
     for line in sys.stdin:
         try:
             line = json.loads(line.rstrip())
             index = line['index']
             image_ids_1 = parse_image_array(line['images_array_1'])
             image_ids_2 = parse_image_array(line['images_array_2'])
-            print(index+','+','.join(map(str, gen_features(image_ids_1, image_ids_2))))
+            print(index + ',' + ','.join(map(str, gen_features(image_ids_1, image_ids_2))))
         except Exception as e:
             logger.exception(e)
